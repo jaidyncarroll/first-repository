ebf7730beb30dc927fc17e3eb7c8bdd9d96d6108
jaidyn carroll <jjgymnastc@gmail.com>
2023-09-04 19:10:40 +0000

    space calculator works!

--- firstrepository.py
+++ firstrepository.py
@@ -1,33 +1,8 @@
-#twist is that you can't use the same thing two times in a row
+print("Welcome to the Space Travel Calculator!")
 
-print("Let's play rock paper scissors! Except with a twist: you can't use the same thing two times in a row.")
+distance = int(input("enter the distance to a celestial object in light years:"))
+speed = int(input("enter the spacecraft speed in fraction of the speed of light:"))
 
-def start_round():
-    your_move = int(input("Choose your move, type 1 for rock, 2 for paper, and 3 for scissors:"))
+time = distance/speed
 
-start_round()
-
-robot_choices = [1, 2, 3]
-robot_choice = random.choice(robot_choices)
-
-def play_game():
-    if robot_choice == your_move:
-        print("tie! keep going")
-        start_round()
-    elif robot_choice == 1 and your_move == 3:
-        print("you lose :(")
-    elif robot_choice == 3 and your_move == 1:
-        print("you win!")
-    elif robot_choice == 1 and your_move == 2:
-        print("you win!")
-    elif robot_choice == 2 and your_move == 1:
-        print("you lose :(")
-    elif robot_choice == 2 and your_move == 3:
-        print("you win!")
-    elif robot_choice == 3 and your_move == 2:
-        print("you lose :(")
-    else:
-        print("something went wrong")
-
-
-play_game()
+print("It would take you", time, "hours to reach your destination, beep bop boop.")
